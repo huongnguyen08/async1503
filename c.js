89965d1080208a84399e27e50bda5b85c4656414
Huong <huongnguyenak96@gmail.com>
2019-04-03 19:09:00 +0700

    function nhan

---
+++ c.js
@@ -0,0 +1,20 @@
+/**
+ * viet 3 function cong/nhan/chia
+ * (3+5)*4/2
+ */
+const request = require('request');
+const URL = 'http://localhost:3000/';
+
+function nhan(a,b,fn){
+    if(isNaN(a) || isNaN(b)) 
+        return fn(null, new Error('Invalid parameter(s)'))
+    const uri = `${URL}nhan/${a}/${b}`
+    request(uri,(error, res, body)=>{
+        if(error) return fn(null, error)
+        return fn(JSON.parse(body).result)
+    })
+}
+nhan(2,3,(result,error)=>{
+    if(error) console.log(error.message)
+    else console.log(result)
+})
